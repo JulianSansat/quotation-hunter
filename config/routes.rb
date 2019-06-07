d49728846320fc4ede9acd913cd93b8c2d1be849
Julian <juliansansat@gmail.com>
2019-06-06 23:51:20 -0300

    added api key to credentials, http client fetching api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/quotations', to: 'quotations#index'
       get '/variation', to: 'quotations#variation'
+      get '/fetch_quotations', to: 'quotations#fetch_quotations'
     end
   end
 end

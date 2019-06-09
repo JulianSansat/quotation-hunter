74cc826f33c988092cbc23098d8830d53a600793
Julian <juliansansat@gmail.com>
2019-06-08 23:24:06 -0300

    re implemented quotations index filters

--- app/controllers/api/v1/quotations_controller.rb
+++ app/controllers/api/v1/quotations_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::QuotationsController < Api::V1::ApiController
+  
   def index
-  	quotations = Quotation.all
+    quotations = QuotationsByDateService.quotations_of(params[:moment], params[:currency_code])
+    render json: {quotations: quotations}, status: 200
   end
 
   def variation
